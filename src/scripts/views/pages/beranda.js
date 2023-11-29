1f002c90272583fa6b58ccb481df5e970d3e32f3
Putra Fajar <116268708+putrafjr@users.noreply.github.com>
2023-11-29 12:49:09 +0700

    Update Kesenian dan Beranda

--- src/scripts/views/pages/beranda.js
+++ src/scripts/views/pages/beranda.js
@@ -1,39 +1,63 @@
 const Beranda = {
-  async render() {
-    const main = document.querySelector('main');
-    main.classList.remove('reset-padding');
-
-    return `
-            <div class="jumbtrone">
-                <div class="jumbtrone-text">
-                    <h2>By creating a visual guide along the way, the designer or developer can get input from the other people involved in the website such as the customer, their manager, and other members of the team. </h2>
-                    <p>By creating a visual guide along the way, the designer or developer can get input from the other people involved in the website such as the customer, their manager, and other members of the team. </p>
-                </div>
-                <div class="jumbtrone-image"></div>
-            </div>
-            <div class="benefit">
-                <hr>
-                <div class="info-group">
-                </div>
-            </div>
-            <div class="trash">
-                <hr>
-                <div class="trash-group">
-                </div>
-            </div>
-      `;
-  },
-
-  async afterRender() {
-    // munculkan hamburger
-    // hide hamburger
-    const hamburger = document.querySelector('.hamburger');
-    hamburger.classList.remove('hide-content');
-
-    // munculkan nav
-    const navigasi = document.querySelector('nav');
-    navigasi.classList.remove('hide-content');
-  },
-};
-
-export default Beranda;
+    async render() {
+      const main = document.querySelector('main');
+      main.classList.remove('reset-padding');
+  
+      return `
+      <div class="Posterhome">
+      <div class="Posterhome-image"></div>
+      <div class="Posterhome-text">
+          <h2>Bali Heritage</h2>
+          <p>Selamat datang di Bali Heritage, sebuah platform untuk memahami lebih dalam tentang kekayaan budaya Bali. </p> 
+          <p>Keindahan budaya Bali yang merangsang minat dalam menjaga dan melestarikan warisan budaya. </p> 
+          <div class="Posterhome-Button">
+              <a href=".#/kesenian" class="Posterhome-Button1"> Kesenian</a>
+              <a href=".#/kesenian" class="Posterhome-Button2"> Event</a>
+          </div>   
+      </div>
+  </div>
+  
+  <hr style="border: 3px solid black; margin: 0;">
+  
+  <div class="contenthome">
+      <div class="contenthome-group">
+          <div class="contenthome-wrap">
+              <div class="contenthome-image"></div>
+              <p></br><span>Kesenian Bali</span></br></br>Kesenian Bali adalah bagian yang tak terpisahkan dari kehidupan dan budaya masyarakat Bali.</br>kesenian Bali terbentuk dari adanya filosofi dan kehidupan masyarakat Bali yang sehari-hari.</p>
+          </div>
+          <div class="contenthome-wrap">
+              <p></br><span>Tradisi Bali</span></br></br>Tradisi Bali adalah warisan budaya yang mencakup banyak aspek kehidupan sehari-hari dan ritual keagamaan.</br>Tradisi tersebut menjadi landasan kuat bagi kehidupan dan identitas budaya masyarakat Bali</p>
+              <div class="contenthome-image"></div>
+          </div>
+          <div class="contenthome-wrap">
+              <div class="contenthome-image"></div>
+              <p></br><span>Pariwisata Bali</span></br></br>Pariwisata Bali merupakan kombinasi dari alam yang indah dan warisan budaya yang kaya.</br> Ini adalah tempat di mana wisatawan dapat menikmati liburan yang menggabungkan keindahan alam</p>
+          </div>
+      </div>
+  </div>
+  
+  <hr style="border: 3px solid black; margin: 0;">
+  
+  <div class="video-container">
+    <h2 id="video">Mengenal Daerah Bali</h2>
+    <video loop autoplay muted class="video-style">
+        <source src="/Video/Opening.mp4" type="video/mp4">
+    </video>
+  </div>
+        `;
+    },
+  
+    async afterRender() {
+      // munculkan hamburger
+      // hide hamburger
+      const hamburger = document.querySelector('.hamburger');
+      hamburger.classList.remove('hide-content');
+  
+      // munculkan nav
+      const navigasi = document.querySelector('nav');
+      navigasi.classList.remove('hide-content');
+    },
+  };
+  
+  export default Beranda;
+  
--- src/scripts/views/pages/kesenian.js
+++ src/scripts/views/pages/kesenian.js
@@ -4,26 +4,43 @@ const Kesenian = {
       main.classList.remove('reset-padding');
   
       return `
-              <div class="trash">
-                  <div class="trash-group">
-                      <div class="trash-wrap">
-                          <div class="trash-image"></div>
-                          <p><span>Pura Bali </span><br>By creating a visual guide along the way, the designer or developer can get input from the other people involved in the website such as the customer, their manager, and other members of the team. Lorem ipsum dolor sit amet consectetur adipiscing elit semper dalar elementum tempus hac tellus libero accumsan. Lorem ipsum dolor sit amet consectetur adipiscing elit semper dalar elementum tempus hac tellus libero accumsan.</p>
-                      </div>
-                      <div class="trash-wrap">
-                          <p><span>Pulau Bali</span><br>By creating a visual guide along the way, the designer or developer can get input from the other people involved in the website such as the customer, their manager, and other members of the team. Lorem ipsum dolor sit amet consectetur adipiscing elit semper dalar elementum tempus hac tellus libero accumsan. Lorem ipsum dolor sit amet consectetur adipiscing elit semper dalar elementum tempus hac tellus libero accumsan.</p>
-                          <div class="trash-image"></div>
-                      </div>
-                      <div class="trash-wrap">
-                          <div class="trash-image"></div>
-                          <p><span>Pantai Bali</span><br>By creating a visual guide along the way, the designer or developer can get input from the other people involved in the website such as the customer, their manager, and other members of the team. Lorem ipsum dolor sit amet consectetur adipiscing elit semper dalar elementum tempus hac tellus libero accumsan. Lorem ipsum dolor sit amet consectetur adipiscing elit semper dalar elementum tempus hac tellus libero accumsan.</p>
-                      </div>
-                      <div class="trash-wrap">
-                          <p><span>Cewek Bali</span><br>By creating a visual guide along the way, the designer or developer can get input from the other people involved in the website such as the customer, their manager, and other members of the team. Lorem ipsum dolor sit amet consectetur adipiscing elit semper dalar elementum tempus hac tellus libero accumsan. Lorem ipsum dolor sit amet consectetur adipiscing elit semper dalar elementum tempus hac tellus libero accumsan.</p>
-                          <div class="trash-image"></div>
-                      </div>
-                  </div>
-              </div>
+        <div class="Kesenian">
+            <div class="Kesenian-text"></br></br>
+                <h2>Tari Kecak</h2>
+                <p>Tari Kecak adalah bentuk tarian tradisional yang berasal dari Bali, Indonesia.</p>
+                <p>Tarian ini terkenal vokal "cak-cak-cak" yang dihasilkan oleh sekelompok penari pria yang duduk melingkar.</p>
+                <p>Tarian Kecak sering dianggap sebagai tarian dramatik karena sering diadaptasi untuk menceritakan cerita epik, terutama dari wiracarita Ramayana.</p> 
+            </div>
+            <div class="Kesenian-image"></div>
+        </div>
+
+        <div class="Kesenian1">
+            <div class="Kesenian1-image"></div>
+            <div class="Kesenian1-text"></br></br>
+                <h2>Ngaben</h2>
+                <p>Ngaben adalah upacara kremasi tradisional di Bali, Indonesia.</p>
+                <p>Upacara ini merupakan salah satu ritual keagamaan Hindu yang sangat penting bagi masyarakat Bali.</p>
+                <p>Ngaben dilakukan untuk membersihkan roh orang yang meninggal dan membantu mereka melewati kehidupan setelah mati.</p> 
+            </div>
+        </div>
+
+        <div class="Kesenian2">
+            <div class="Kesenian2-text"></br></br>
+                <h2>Ogoh-Ogoh</h2>
+                <p>Ogoh-ogoh adalah patung raksasa yang merupakan bagian penting dari perayaan Hari Raya Nyepi di Bali, Indonesia.</p> 
+                <p>Ogoh-ogoh memiliki makna simbolis yang mendalam. Patung ini sering kali mewakili roh jahat atau kejahatan dalam tradisi Hindu.</p> 
+            </div>
+            <div class="Kesenian2-image"></div>
+        </div>
+
+        <div class="Kesenian3">
+            <div class="Kesenian3-image"></div>
+            <div class="Kesenian3-text"></br></br>
+                <h2>Omed-omedan</h2>
+                <p>Omed-omedan adalah tradisi unik yang dilakukan oleh masyarakat desa Banjar Kaja di Bali, Indonesia.</p> 
+                <p>Tradisi ini menjadi bagian dari perayaan hari raya Saraswati, yang dirayakan setiap 210 hari sekali dalam kalender Pawukon.</p> 
+            </div>
+        </div>
         `;
     },
   
