c19f5d971d1f060418e37586f9f5d97239bfda72
Putra Fajar <116268708+putrafjr@users.noreply.github.com>
2023-12-11 13:01:18 +0700

    Update Function Login

--- src/scripts/views/pages/login.js
+++ src/scripts/views/pages/login.js
@@ -1,42 +1,68 @@
 const Login = {
-    async render() {
-      const main = document.querySelector('main');
-      main.classList.remove('reset-padding');
-  
-      return `
-      <div class="Login-page">
+  async render() {
+    const main = document.querySelector('main');
+    main.classList.remove('reset-padding');
+
+    return `
+    <div class="Login-page">
       <div class="horizontal-form">
-      <div class="heading"></div>
-      <div class="form-body">
-        <form action="" method="post">
-          <div class="form-group">
-            <label for="fname">Username:</label>
-            <input type="text" class="form-control" id="fname" name="username" placeholder="Username">
-          </div>
-          <div class="form-group">
-            <label for="lname">Password:</label>
-            <input type="password" class="form-control" id="lname" name="password" placeholder="Password">
-          </div></br>
-          <div class="button_box">
-            <button type="submit" name="loginsubmit" class="btn"><a href=".#/admin">LOGIN</a></button>
-          </div>
-        </form>
+        <div class="heading"></div>
+        <div class="form-body">
+        <div class="login-body">
+          <form>
+              <label for="#username-admin">Username</label>
+              <input id="username-admin" type="text" required placeholder="Masukan Username">
+              <label for="#pass-admin">Password</label>
+              <input id="pass-admin" type="password" required placeholder="Masukan Password">
+          </form>
+        <button id="submit-admin">Masuk</button>
+        <p id="wrongName" class="hide-content">Username Salah!</p>
+        <p id="wrongPass" class="hide-content">Password Salah!</p>
+        <p id="wrongPassName" class="hide-content">Username Dan Password Salah!</p>
+        </div>
+        </div>
       </div>
-      </div>
-      </div>
-        `;
-    },
-  
-    async afterRender() {
-      // munculkan hamburger
-      // hide hamburger
-      const hamburger = document.querySelector('.hamburger');
-      hamburger.classList.remove('hide-content');
-  
-      // munculkan nav
-      const navigasi = document.querySelector('nav');
-      navigasi.classList.remove('hide-content');
-    },
-  };
-  
-  export default Login;
+    </div>
+      `;
+  },
+
+  async afterRender() {
+    // hide hamburger
+    const hamburger = document.querySelector('.hamburger');
+    hamburger.classList.add('hide-content');
+
+    // hide nav
+    const navigasi = document.querySelector('nav');
+    navigasi.classList.add('hide-content');
+
+    // konfigurasi inputan
+    const username = document.querySelector('#username-admin');
+    const password = document.querySelector('#pass-admin');
+    const button = document.querySelector('#submit-admin');
+
+    const wrongName = document.querySelector('#wrongName');
+    const wrongPass = document.querySelector('#wrongPass');
+    const wrongPassName = document.querySelector('#wrongPassName');
+
+    button.addEventListener('click', (e) => {
+      e.stopPropagation();
+      if (username.value !== 'admin' && password.value === 'admin123') {
+        wrongName.classList.remove('hide-content');
+        wrongPass.classList.add('hide-content');
+        wrongPassName.classList.add('hide-content');
+      } else if (username.value === 'admin' && password.value !== 'admin123') {
+        wrongName.classList.add('hide-content');
+        wrongPass.classList.remove('hide-content');
+        wrongPassName.classList.add('hide-content');
+      } else if (username.value !== 'admin' && password.value !== 'admin123') {
+        wrongName.classList.add('hide-content');
+        wrongPass.classList.add('hide-content');
+        wrongPassName.classList.remove('hide-content');
+      } else {
+        window.location.href = '#/admin';
+      }
+    });
+  },
+};
+
+export default Login;
